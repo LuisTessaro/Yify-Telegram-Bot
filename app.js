e4373e1b0cc8ce6632f69bbb8ed017a6ae8a1139
unknown <lh.pedrosot@hotmail.com>
2020-04-18 01:46:46 -0300

    barebones webserver for http hook

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ require('dotenv').config()
 const config = require('./config')
 const token = process.env.BOT_TOKEN
 
+const express = require('express')
+const app = express()
+
+app.listen(process.env.PORT, () => console.log('[INFO] Server Running'))
+
 const bot = config.setUpBot(token)
 bot.launch()
 
